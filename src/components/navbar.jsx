6849f381e3c123d3d27744cf01b89b8e771844c6
rakib <you@example.com>
2026-09-24 16:55:29 +0800

    fix: update z-index values for StackSidebar and Navbar components for improved layering

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,7 +3,7 @@ import logo from "../assets/logo-text.png";
 function Navbar() {
   return (
     <>
-      <div className="navbar bg-base-100 shadow-sm sticky top-0 z-500</>">
+      <div className="navbar bg-base-100 shadow-sm sticky top-0 z-9999</>">
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
